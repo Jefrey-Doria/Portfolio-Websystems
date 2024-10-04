7034bd1a0878823e19862a8b862ca3433c4036ce
sinsXenvy (Jefrey Doria) <jefreydoria1999@gmail.com>
2024-10-04 18:35:18 +0800

    Minor Bugfixes
    - HTML class fixes

--- js/app.js
+++ js/app.js
@@ -53,20 +53,6 @@ $(function () {
   }
   requestAnimationFrame(raf);
 
-  // Parallax (apply parallax effect to any element with a data-speed attribute)
-  gsap.to("[data-speed]", {
-    y: (i, el) =>
-      (1 - parseFloat(el.getAttribute("data-speed"))) *
-      ScrollTrigger.maxScroll(window),
-    ease: "none",
-    scrollTrigger: {
-      start: 0,
-      end: "max",
-      invalidateOnRefresh: true,
-      scrub: 0,
-    },
-  });
-
   // --------------------------------------------- //
   // Scroll Animations
   // --------------------------------------------- //
